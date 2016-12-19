137f30bd3b38950bde9bda36a76f9eb9ecb25529
Mukul Garg <gargmukku07@gmail.com>
2016-12-19 11:23:57 +0000

    Tag EC2

--- GUI.py
+++ GUI.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 # import templates
 
-from flask import render_template
+from flask import render_template, request
 import boto3
 import botocore
+from botocore import exceptions
 
-from flask import request
 
 app = Flask(__name__)
 
@@ -18,8 +18,16 @@ def index():
 access_key = ''
 secret_key = ''
 region = ''
+ami_id = 'ami-cdbdd7a2'
+userdata = """#Cloud-config
 
+runcmd:
+ - mkdir /app
+ - touch /app/mukul.txt
 
+packages:
+ - httpd
+"""
 @app.route('/aws', methods=['GET','POST'])
 def aws():
     if request.method == 'POST':
@@ -47,17 +55,33 @@ def aws_ec2():
         environment = request.form['environment']
         key_name = request.form['key_name']
         security_group = request.form['security_group']
+        # No use if security group provided.
         subnet_id = request.form['subnet_id']
         instance_type = request.form['instance_type']
         instance_count = int(request.form['instance_count'])
         ec2 = boto3.resource('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
-        instance = ec2.create_instances(
-            ImageId='ami-cdbdd7a2',
+        instances = ec2.create_instances(
+            ImageId=ami_id,
             MinCount=1,
             MaxCount=instance_count,
             KeyName=key_name,
+            SecurityGroups=[
+                security_group,
+            ],
+            UserData=userdata,
             InstanceType=instance_type
         )
+        mytag = [{
+            'Key': 'Name',
+            'Value': environment + '_' + server_type
+        }]
+        for instance in instances:
+            ec2.create_tags(
+                Resources=[
+                    instance.id,
+                ],
+                Tags=mytag
+            )
         return render_template('index.html', aws_ec2=environment)
     else:
         return render_template('aws_ec2.html')
