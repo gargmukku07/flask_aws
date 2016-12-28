952c71182c42765336b6e70c4e34c672941d4dfd
Mukul Garg <gargmukku07@gmail.com>
2016-12-28 18:00:55 +0000

    Added Static files and modify GUI

--- GUI.py
+++ GUI.py
@@ -8,26 +8,34 @@ import botocore
 from botocore import exceptions
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def index():
-    return render_template('aws.html')
+    return render_template('index.html')
 
 access_key = ''
 secret_key = ''
 region = ''
 ami_id = 'ami-cdbdd7a2'
-userdata = """#Cloud-config
+pmasterData = """#Cloud-config
+
+write_files:
+ - path: '/tmp/puppet_server.sh'
+   owner: root:root
+   permission: '0755'
+   content: |
+      #!/usr/bin/bash
+      PUPPET_BASE="/opt/puppetlabs"
+      PUPPET_HIERA_FILE="/etc/puppetlabs/puppet/hiera.yaml"
 
 runcmd:
- - mkdir /app
- - touch /app/mukul.txt
+ - touch /tmp/muu.txt
 
-packages:
- - httpd
 """
+
+
 @app.route('/aws', methods=['GET','POST'])
 def aws():
     if request.method == 'POST':
@@ -68,7 +76,7 @@ def aws_ec2():
             SecurityGroups=[
                 security_group,
             ],
-            UserData=userdata,
+            UserData=pmasterData,
             InstanceType=instance_type
         )
         mytag = [{
@@ -85,6 +93,24 @@ def aws_ec2():
         return render_template('index.html', aws_ec2=environment)
     else:
         return render_template('aws_ec2.html')
+
+
+@app.route('/aws_vpc', methods=['GET','POST'])
+def aws_vpc():
+    if request.method == 'POST':
+        vpc_name=request.form['vpc_name']
+        cidr_block = request.form['cidr_block']
+
+        ec2 = boto3.resource('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
+        vpc = ec2.create_vpc(
+            CidrBlock = cidr_block,
+            InstanceTenancy = 'default',
+            AmazonProvidedIpv6CidrBlock = 'False'
+        )
+
+        return render_template('index.html')
+    else:
+        return render_template('aws_vpc.html')
 
 if __name__ == '__main__':
     app.run()
